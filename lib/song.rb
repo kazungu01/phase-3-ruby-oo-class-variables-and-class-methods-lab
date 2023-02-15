9a5e7efc1edef6fff63c387d78aedc430dd61e9d
hassan kazungu <hassankazungu01@gmail.com>
2023-02-15 21:34:32 +0300

    done

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song 
+    attr_accessor :artist, :genre, :name
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << self.genre
+        @@artists << self.artist
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.map {|genre| genre}.uniq
+    end
+
+    def self.artists
+        @@artists.map {|artist| artist}.uniq
+    end
+
+    def self.genre_count 
+        @@genres.tally
+        
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+end
